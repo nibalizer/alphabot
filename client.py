a2b848a7e8e11f3324d1416408b269a3033b7bc5
Derek Hurley <dzhurley@gmail.com>
2011-04-16 20:09:04 -0700

    major clean with py(lint|flakes), plus my first decorator

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ This module contains the TrailBot class for IRC actions, TrailBotFactory to
 set up the trailbot protocol, and TrailBotContextFactory for ssl support.
 """
 
-from OpenSSL import SSL
 from twisted.words.protocols import irc
 from twisted.internet import protocol, ssl
 from twisted.python import rebuild
@@ -17,6 +16,7 @@ import docs
 import voice
 import random
 
+
 class TrailBot(irc.IRCClient):
     """Main bot interface with IRC happenings
 
@@ -24,10 +24,11 @@ class TrailBot(irc.IRCClient):
 
     """
 
+    @property
     def _get_nickname(self):
         return self.factory.nickname
-    # stores trailbot's nick in TrailBot
-    nickname = property(_get_nickname)
+
+    nickname = _get_nickname
 
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
@@ -42,7 +43,7 @@ class TrailBot(irc.IRCClient):
 
     def userKicked(self, kickee, channel, kicker, message):
         self.msg(channel, kicker + random.choice(voice.saw_kick))
-    
+
     def userJoined(self, user, channel):
         """greetings for user joining each channel"""
         self.msg(channel, user + random.choice(voice.user_joined[channel]))
@@ -52,33 +53,34 @@ class TrailBot(irc.IRCClient):
 
     def privmsg(self, user, channel, msg):
         """Handles user messages from channels
-        
+
         This hooks every privmsg sent to the channel and sends the commands off
         to cmds.dispatch to process, then replies to the channel accordingly.
-        
+
         The @reload command needs to be handled here though, as it allows edits
         to be made to trailbot without disconnecting from IRC. It should get a
         list from cmds.dispatch if it needs to pm someone, otherwise it gets a
         string back and msgs the channel.
 
         """
-        user = user.split('!',1)[0]
-        
+        user = user.split('!', 1)[0]
+
         if msg == '@reload':
             rebuild.rebuild(cmds)
             rebuild.rebuild(docs)
             rebuild.rebuild(voice)
             self.msg(channel, 'reloaded and ready to go')
         else:
-            reply = cmds.dispatch(user, channel, msg)
+            reply = cmds.dispatch(user, msg)
 
             if type(reply) is types.StringType:
-                self.msg(channel, reply) 
+                self.msg(channel, reply)
             else:
                 if len(reply):
                     self.msg(channel, reply[0])
                     for trip in reply[1:]:
-                        self.msg(user, trip)                
+                        self.msg(user, trip)
+
 
 class TrailBotFactory(protocol.ClientFactory):
     """Subclass of ClientFactory for trailbot protocol"""
@@ -97,6 +99,7 @@ class TrailBotFactory(protocol.ClientFactory):
     def clientConnectionFailed(self, connector, reason):
         # give up
         print 'connection failed',
+
 
 class TrailBotContextFactory(ssl.ClientContextFactory):
     """Subclass of ClientContextFactory for ssl connection"""
